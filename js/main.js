e618015780a4ae8b4ad2636691d2a1fe520f0321
John D'Oriocourt <kibner@gmail.com>
2023-01-21 11:19:54 -0600

    - added csv parser library - made some styling changes for table - added some test data in code for quick testing of csv parser

--- js/main.js
+++ js/main.js
@@ -2,11 +2,27 @@
   // readystatechange as event listener to insert or modify the DOM before DOMContentLoaded
   document.addEventListener('readystatechange', event => {
     if (event.target.readyState === 'complete') {
-      _initializeApp();
+      _initialize_app();
     }
   });
 
-  function _initializeApp() {
-    t
+  function _initialize_app() {
+    const TEST_ACTUAL_TRADES_CSV = `
+id,player
+1,nick van exel
+2,jose alvarado
+`
+
+    const TEST_PREDICTIONS_CSV = `
+id,user,player
+1,derek fisher fan,nick van exel
+2,derek fisher fan,robert horry
+3,derek fisher fan,jordan hill
+4,derek fisher fan,chris mihm
+5,austin rivers enjoyer,jose alvarado
+6,austin rivers enjoyer,robin lopez
+7,austin rivers enjoyer,jrue holiday
+8,austin rivers enjoyer,jarrett jack
+`
   }
 })();
