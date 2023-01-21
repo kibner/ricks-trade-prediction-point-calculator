27c5c09fa990f829b249a6e3ae418a73344aeb8b
John D'Oriocourt <kibner@gmail.com>
2023-01-21 09:08:40 -0600

    - added initial html elements - added site.css file - added some skeleton code in main.js so that desired code can run after page loads

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function () {
+  // readystatechange as event listener to insert or modify the DOM before DOMContentLoaded
+  document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === 'complete') {
+      _initializeApp();
+    }
+  });
+
+  function _initializeApp() {
+    t
+  }
+})();
