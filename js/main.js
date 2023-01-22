7337d698d8c249e6adde6c337364f7a92b99ca7d
John D'Oriocourt <kibner@gmail.com>
2023-01-21 21:41:51 -0500

    - refactored some names - added a hardcoded calculation function

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@ import {parse} from "csv-parse/lib/sync";
   })
 
   function _initialize_app() {
-    const TEST_ACTUAL_TRADES_CSV = `
+    const TEST_COMPLETED_TRADES_CSV = `
 id,player
 1,nick van exel
 2,jose alvarado
 `
 
-    const TEST_PREDICTIONS_CSV = `
+    const TEST_PREDICTED_TRADES_CSV = `
 id,user,player
 1,derek fisher fan,nick van exel
 2,derek fisher fan,robert horry
@@ -27,8 +27,9 @@ id,user,player
 8,austin rivers enjoyer,jarrett jack
 `
 
-    const PARSED_ACTUAL_TRADES = get_parsed_csv(TEST_ACTUAL_TRADES_CSV)
-    const PARSED_PREDICTIONS = get_parsed_csv(TEST_PREDICTIONS_CSV)
+    const PARSED_COMPLETED_TRADES = get_parsed_csv(TEST_COMPLETED_TRADES_CSV)
+    const PARSED_PREDICTED_TRADES = get_parsed_csv(TEST_PREDICTED_TRADES_CSV)
+    const RESULTS = get_calculated_results(PARSED_COMPLETED_TRADES, PARSED_PREDICTED_TRADES)
   }
 
   function get_parsed_csv(csv_string) {
@@ -37,4 +38,49 @@ id,user,player
       skip_empty_lines: true
     })
   }
-})();
+
+  function get_calculated_results(completed_trades, predicted_trades) {
+    return [
+      {
+        user: `Derek Fisher Fan`,
+        prediction_results: [
+          {
+            player: `Nick Van Exel`,
+            points: 5
+          },
+          {
+            player: `Robert Horry`,
+            points: 2
+          },
+          {
+            player: `Jordan Hill`,
+            points: 2
+          }, {
+            player: `Chris Mihm`,
+            points: 1
+          }
+        ]
+      },
+      {
+        user: `Austin Rivers Enjoyer`,
+        prediction_results: [
+          {
+            player: `Jose Alvarado`,
+            points: 2
+          },
+          {
+            player: `Robin Lopez`,
+            points: 1
+          },
+          {
+            player: `Jrue Holiday`,
+            points: 2
+          },
+          {
+            player: `Jarret Jack`,
+            points: 1
+          }
+        ]
+      }]
+  }
+})()
