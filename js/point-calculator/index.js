15659da8c45b745b6cb3e6e093bd7f6183193ece
John D'Oriocourt <kibner@gmail.com>
2023-01-28 09:33:09 -0600

    grouped predictions for easier processing

--- js/point-calculator/index.js
+++ js/point-calculator/index.js
@@ -1,23 +1,23 @@
-import {TEST_DATA__CALCULATED_RESULTS} from '../test-data';
-
 // +01: For a correctly named player.
 // +03 Bonus point if between (and including, to clarify) 5 and 1 other people mentioned this player
 // +06: If you nail at least three players.
 // +09: Bonus points if no one else mentioned this player.
 
-const INITIAL_PICK_FREQUENCY_VALUE = 0;
+import {TEST_DATA__CALCULATED_RESULTS} from '../test-data';
 
-const HAS_MATCHING_PLAYER = function (prediction, completed_trade)
+const HAS_MATCHING_PLAYER = function (predicted_trade, completed_trade)
 {
-  return prediction.player === completed_trade.player;
+  return predicted_trade.player === completed_trade.player;
 };
 
 const GET_PICK_FREQUENCY = function (predicted_trades, completed_trade)
 {
+  const INITIAL_PICK_FREQUENCY_VALUE = 0;
+
   return predicted_trades.reduce(
-    function (accumulator, current_prediction)
+    function (accumulator, current_predicted_trade)
     {
-      if (HAS_MATCHING_PLAYER(current_prediction, completed_trade))
+      if (HAS_MATCHING_PLAYER(current_predicted_trade, completed_trade))
       {
         accumulator = accumulator + 1;
       }
@@ -39,11 +39,27 @@ const SET_PICK_FREQUENCY_FOR_COMPLETED_TRADES = function (completed_trades, pred
   return completed_trades;
 };
 
+function GET_GROUPED_PREDICTED_TRADES(predicted_trades)
+{
+  const INITIAL_GROUP_PREDICTED_TRADE_VALUE = {};
+
+  return predicted_trades.reduce(
+    function (accumulator, current_predicted_trade)
+    {
+      const KEY = current_predicted_trade.user;
+      const CURRENT_GROUP = accumulator[KEY] ?? [];
+
+      return {...accumulator, [KEY]: [...CURRENT_GROUP, current_predicted_trade.player]};
+    }, INITIAL_GROUP_PREDICTED_TRADE_VALUE,
+  );
+}
+
 export const GET_CALCULATED_RESULTS = function (completed_trades, predicted_trades)
 {
   completed_trades = SET_PICK_FREQUENCY_FOR_COMPLETED_TRADES(completed_trades, predicted_trades);
+  const GROUPED_PREDICTED_TRADES = GET_GROUPED_PREDICTED_TRADES(predicted_trades);
 
-  console.log(completed_trades);
+  console.log(GROUPED_PREDICTED_TRADES);
 
   return TEST_DATA__CALCULATED_RESULTS;
 };
