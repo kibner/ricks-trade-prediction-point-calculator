7f8410017c01f856f12a596c937f13427eafd723
John D'Oriocourt <kibner@gmail.com>
2023-02-09 17:00:02 -0600

    fixed issue when quoted fields in csv had whitespace in them

--- js/parse-data/index.js
+++ js/parse-data/index.js
@@ -3,11 +3,28 @@ import {parse} from 'csv-parse/lib/sync';
 export const COMPLETED_TRADE_COLUMNS = ['id', 'player'];
 export const PREDICTED_TRADE_COLUMNS = ['id', 'user', 'player'];
 
+const TRIM_DATA = function (data)
+{
+  const KEYS = Object.keys(data);
+
+  KEYS.map(function(key){
+    if(typeof data[key] === 'string' || data[key] instanceof String){
+      data[key] = data[key].trim();
+    }
+  });
+
+  return data
+}
+
 export const GET_PARSED_CSV = function (csv_string, columns)
 {
-  return parse(csv_string, {
+  const PARSED_CSV = parse(csv_string, {
     columns: columns,
     skip_empty_lines: true,
     from_line: 2,
+  });
+
+  return PARSED_CSV.map(function (data) {
+    return TRIM_DATA(data);
   });
 };
